edce7fc4f71e02b3d7feb472ef5d95d31a759ad3
Chee Yuan <chong-1997@hotmail.com>
2019-09-12 12:14:22 +0800

    My own stuff

--- puzzle_A1_xx_1.py
+++ puzzle_A1_xx_1.py
@@ -1,17 +1,6 @@
 import os
 import sys
-import copy
 
-moveNum = {1:"RIGHT", 2:"LEFT", 3:"UP", 4:"DOWN"}
-moveStr = {"RIGHT":1, "LEFT":2, "UP":3, "DOWN":4}
-
-class Node(object):
-    def __init__(self, state, action_history, gn, hn):
-        # You may add more attributes as necessary
-        self.state = state
-        self.action_history = action_history
-        self.gn = gn
-        self.hn = hn
 
 class Puzzle(object):
     def __init__(self, init_state, goal_state):
@@ -20,97 +9,11 @@ class Puzzle(object):
         self.goal_state = goal_state
         self.actions = list()
         self.solvable = True
-        self.priorityQueue = list()
 
     def solve(self):
         # TODO: Write your code here
         # return: a list of actions like: ["UP", "DOWN"]
-        startNode = Node(init_state, list(), 0, self.heuristicFunction(init_state))
-        self.priorityQueue.push(startNode)
-        while True:
-            node = self.pop()
-            if self.isGoalNode(node):
-                break
-            new_frontier_nodes = self.generateFrontierNode(node)
-            self.priorityQueue.pushAll(new_frontier_nodes)
-        return node.action_history
-            
-    # h1 heuristic: number of misplaced tiles
-    def calcNumMisplacedTiles(self, curr_state):
-        num = 1
-        numMisplaced = 0
-        for i in range(3):
-            for j in range(3):
-                if (curr_state[i][j] != num):
-                    numMisplaced += 1
-                num +=1
-        if (curr_state[2][2] == 0):
-            numMisplaced -= 1
-        return numMisplaced
-
-    # h2 heuristic: total Manhattan-Distance
-    def calcDistanceSum(self, state):
-        sum = 0
-        for i in range(9):
-            sum += self.getDistance(i, state)
-        return sum
-
-    def getDistance(self, number, state):
-        indexGoal = self.getIndex(number, goal_state)
-        indexCurrentState = self.getIndex(number, state)
-        distance = abs(indexGoal[0] - indexCurrentState[0]) + abs(indexGoal[1] - indexCurrentState[1])
-        return distance
-
-    def getIndex(self, number, state):
-        for idxOuter, row in enumerate(state):
-            for idxInner, elem in enumerate(row):
-                if elem == number:
-                    return (idxOuter, idxInner)
-
-    # h3 = (h1 + h2)/2
-    def heuristicFunction(self, state):
-        return (self.calcDistanceSum(state) + self.calcNumMisplacedTiles(state)) / 2
-
-    # Calculate f(n) = g(n) + h3(n)
-    def calc_fn(self, node):
-        return len(node.action_history) + self.heuristicFunction(node.state)
-
-    # State-related methods
-    def findPossibleMoves(self, state):
-        moves = list()
-        zeroRow = 0
-        zeroCol = 0
-        for i in range(3):
-            for j in range(3):
-                if (state[i][j] == 0):
-                    zeroRow = i
-                    zeroCol = j
-        if (zer)
-
-    def isGoalNode(self, node):
-        return self.calcNumMisplacedTiles(node.state) == 0
-
-    def generateFrontierNode(self, node):
-        possibleMoves = findPossibleMoves(node.state)
-        for move in possibleMoves:
-            newState = self.getNextState(node.state, move)
-
-    def getNextState(self, state, move):
-        indexOfZero = self.getIndex(0, state)
-        nextState = copy.deepcopy(state)
-        if(move == moveStr.RIGHT):
-            nextState[indexOfZero[0]][indexOfZero[1]] = nextState[indexOfZero[0]][indexOfZero[1] - 1]
-            nextState[indexOfZero[0]][indexOfZero[1] - 1] = 0
-        elif(move == moveStr.LEFT):
-            nextState[indexOfZero[0]][indexOfZero[1]] = nextState[indexOfZero[0]][indexOfZero[1] + 1]
-            nextState[indexOfZero[0]][indexOfZero[1] + 1] = 0
-        elif(move == moveStr.DOWN):
-            nextState[indexOfZero[0]][indexOfZero[1]] = nextState[indexOfZero[0] - 1][indexOfZero[1]]
-            nextState[indexOfZero[0] - 1][indexOfZero[1]] = 0
-        else:
-            nextState[indexOfZero[0]][indexOfZero[1]] = nextState[indexOfZero[0] + 1][indexOfZero[1]]
-            nextState[indexOfZero[0] + 1][indexOfZero[1]] = 0
-        return nextState
+        pass
 
     # You may add more (helper) methods if necessary.
     # Note that our evaluation scripts only call the solve method.
@@ -150,3 +53,10 @@ if __name__ == "__main__":
     with open(sys.argv[2], 'a') as f:
         for answer in ans:
             f.write(answer+'\n')
+
+
+
+
+
+
+
